5c454b0c1a96f3bdcb6bee55c3355d3906e7fc27
Jefferson Seno <jefferson.seno@datacake.com.br>
2022-04-12 11:52:23 -0300

    Atualização da assinatura

--- cypress/integration/teste_assinatura.js
+++ cypress/integration/teste_assinatura.js
@@ -1,51 +1,5 @@
-/* 
---- Alguns Comandos para o cypress ----
-
-cy.get busca elemento
-cy.visit busca url
-cy.fixture busca arquivo
-cy.request busca requisição
-cy.wait busca espera
-cy.screenshot busca captura de tela
-cy.get().should() busca verificação
-cy.get().click() busca clique
-cy.get().type() busca digitação
-cy.get().clear() busca limpa
-cy.get().should('contain') busca verificação
-cy.get().should('have.class') busca verificação
-cy.get().should('be.visible') busca verificação
-cy.get().should('not.be.visible') busca verificação
-cy.get().should('not.exist') busca verificação
-cy.pause() busca pausa
-cy.press('Enter') busca pressionar tecla
-.type informa o conteudo do elemento
-.click informa que o elemento foi clicado
-.should() informa que o teste vai ser realizado
-.contains informa que o elemento deve conter o texto
-.and informa que o teste vai ser realizado
-cy.visit informa que o teste vai ser realizado
-.label seleciona o label
-
-*/
-
-/* 
----------Pagina 1---------
-CPF = bfc_taxvat
-Nome Completo = bfc_name
-Email = bfc_email
-Telefone celular = mobile_celular
-Cep Instalação = bfc_postalCode
-Número da residência = numberOfAddress
-Tipo de acesso Label = bfc_typeOfAccess
-Já é cliente Label = bfc_typeOfCustomer
-Aceite de Termos = bfc_lgpd_disclaimer_label
-Continuar Assinatura = bfc_continuar
-
-------------------------------
-
-*/
 // Importando cypress-file-upload para fazer upload de arquivos
-// import 'cypress-file-upload';
+import 'cypress-file-upload';
 
 /* Adicionando comando Intelessense */
 /// <reference types="cypress" />
@@ -54,22 +8,22 @@ Continuar Assinatura = bfc_continuar
 describe('Devisp Assine', () => {
     it('Fazer assinatura, no devisp', () => {
         
-        // const filepath1 = 'img/#01doc_selfie001.png'; // filepath do arquivo que será enviado
-        // const filepath2 = 'img/#02doc_selfie002.png'; // filepath do arquivo que será enviado
-        // const filepath3 = 'img/#03doc_selfie003.png'; // filepath do arquivo que será enviado
+        const filepath1 = '#01doc_selfie001.png'; // Crio uma constante para a o arquivo que fica dentro da pasta fixtures
+        const filepath2 = '#02doc_selfie002.png'; // Crio uma constante para a o arquivo que fica dentro da pasta fixtures
+        const filepath3 = '#03doc_selfie003.png'; // Crio uma constante para a o arquivo que fica dentro da pasta fixtures
 
         //cy.visit para ir para a url
         cy.visit('https://devisp.datacake.com.br')
 
-        cy.clearCookies()
-        cy.clearLocalStorage
+        cy.clearCookies() // limpa cookies
+        cy.clearLocalStorage() // limpa local storage
 
         // Página 1 do assine
         //cy.get para buscar o elemento que no caso são os IDs dos campos, type para digitar o conteudo, click para clicar e clear para limpar o campo.
-        cy.get('[id=bfc_taxvat]').type('935.078.950-72') //CPF
-        cy.get('#bfc_name').type('Teste de Assinatura') //Nome Completo
+        cy.get('#bfc_taxvat').type('935.078.950-72') //CPF //TODO:Preciso de um método para pegar um CPF aleatorio
+        cy.get('#bfc_name').type('Teste de Assinatura Cypress') //Nome Completo
         cy.get('#bfc_email').type('testes@datacake.com.br') //Email
-        cy.get('#mobile_celular').type('11 99999-9999') //Telefone celular
+        cy.get('#mobile_celular').type('11 99999-9999') //TODO:Preciso de um método para pegar um celular aleatorio
         cy.get('#bfc_postalCode').type('18680410') //Cep Instalação
         cy.get('#bfc_numberOfAddress').type('410') //Número da residência
         cy.get('#bfc_typeOfAccess').select('Residencial') //Tipo de acesso selecionado
@@ -79,7 +33,7 @@ describe('Devisp Assine', () => {
         cy.get('#bfc_continuar').click() //Continuar Assinatura
 
         // Página 2 do assine
-        cy.get('#net_add_plan_52').click() //Adicionar Plano
+        cy.get('#net_add_plan_52').click() //TODO: Preciso arrumar um jeito de buscar um o primeniro plano disponivel
         cy.wait(2000) //Espera 2 segundos
         cy.get('#summary-continuar-assinatura').click() //Continuar Assinatura
 
@@ -97,14 +51,43 @@ describe('Devisp Assine', () => {
         // Página 4 do assine
         cy.get('#s02_select_contractTime').select('1 ano') //Fidelidade de 1 ano selecionado
         cy.get('#label_dueDay_10').click() //Dia de vencimento selecionado
-        cy.get(':nth-child(4) > :nth-child(1) > .custom-control > .custom-control-label > span').click() //Forma de pagamento selecionado
-        // cy.get('#s02-open-docs-modal').click() //Abrir documentos
-        // cy.get('#send_selfie').uploadFile('D:\\screenshots\\#01doc_selfie001.png') //Enviar selfie   #TODO preciso descobrir como enviar arquivos no cypress!
-        // cy.get('#send_selfie').attachFile(filepath1) //Enviar selfie
-        // cy.get('#file-submit').click()
-        // cy.get('#uploaded-files').contains('#01doc_selfie001.png')
+        cy.get('#payment-multi-3').click() //Forma de pagamento selecionado
+        
+        cy.get('#s02-open-docs-modal').click() //Abrir documentos
+        cy.get('#send_selfie').click() //Enviar selfie
+        cy.get('input[type="file"]').attachFile(filepath1) //Enviar selfie
+        cy.wait(8000) //Espera 8 segundos
+        cy.get('#s02-modal-document-page-0').click() //Abrir documentos
+        
+        cy.get('#send_selfie').click() //Enviar selfie
+        cy.get('input[type="file"]').attachFile(filepath2) // Foto do documento
+        cy.wait(8000) //Espera 8 segundos
+        cy.get('#s02-modal-document-page-1').click() //Abrir documentos
+        
+        cy.get('#send_selfie').click() //Enviar selfie
+        cy.get('input[type="file"]').attachFile(filepath3) //Enviar comprovante de residência
+        cy.wait(8000) //Espera 8 segundos
+        cy.get('#s02-finalizar-btn-2').click() //Finalizar assinatura
+
+
         cy.get('#label_term-7').click() //Termos de uso selecionado
         cy.get('#s02-confirmar-assinatura').click() //Confirmar Assinatura
+        
+        cy.get('#restar_subscription').click() // Nova Assinatura
+                
+        // Verificando se a assinatura foi corretamente no Tarefas:
+        cy.visit('https://devisp.datacake.com.br/admin/#/task-result') //TODO: Preciso aprender como faz login automatico no wda.
+        
+        cy.wait(10000) //Espera 10 segundos
+        
+        //Testando boletos
+        cy.visit('https://devisp.datacake.com.br/boleto')
+        cy.get('#boleto-taxvat').type('935.078.950-72') //CPF //TODO:Preciso de um método para pegar um CPF aleatorio
+        cy.get('#boleto-submit').click() //Enviar boleto
+        
+        // Limpando os cookies e local storage
+        cy.clearCookies() // limpa cookies
+        cy.clearLocalStorage() // limpa local storage
     });
     
 });
